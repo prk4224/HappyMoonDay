f8a866c957858cc575d4ab2a6e7790c7083e85d5
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-04 20:34:28 +0900

    사용하지 않는 import 제거

--- app/src/main/java/com/korea/happymoonday/MainActivity.kt
+++ app/src/main/java/com/korea/happymoonday/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.korea.happymoonday
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.korea.happymoonday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
