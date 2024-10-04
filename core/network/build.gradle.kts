d3522e968c5bbb32d96222074e3ccd367444ea53
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-04 23:20:57 +0900

    hilt 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -37,7 +38,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.dagger.hilt)
+    implementation(libs.dagger.hilt.compiler)
 }
