f7eed07ac69fe2af322803aa733c751a28cd9f7a
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-04 20:56:43 +0900

    홈 화면 모듈 분리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":feature:home"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -41,10 +41,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
 }
