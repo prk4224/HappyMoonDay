08979d4cd13502b516c76aad0d2139ffb9ab57b5
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-04 20:32:59 +0900

    스플래쉬 화면 딜레이 1.5 초 추가

--- app/src/main/java/com/korea/happymoonday/SplashActivity.kt
+++ app/src/main/java/com/korea/happymoonday/SplashActivity.kt
@@ -1,20 +1,35 @@
 package com.korea.happymoonday
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.korea.happymoonday.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            delay(SPLASH_DELAY_MS)
+            moveMain()
         }
     }
+
+    private fun moveMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val SPLASH_DELAY_MS = 1500L
+    }
 }
+
