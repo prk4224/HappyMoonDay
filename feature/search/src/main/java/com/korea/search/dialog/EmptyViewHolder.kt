91875b1deaa202943570b657eb190ca5249d00ae
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-10 08:08:25 +0900

    Code Clean Up

--- feature/search/src/main/java/com/korea/search/dialog/EmptyViewHolder.kt
+++ feature/search/src/main/java/com/korea/search/dialog/EmptyViewHolder.kt
@@ -4,5 +4,5 @@ import androidx.recyclerview.widget.RecyclerView
 import com.korea.search.databinding.EmptyItemBinding
 
 internal class EmptyViewHolder(
-    private val binding: EmptyItemBinding
-): RecyclerView.ViewHolder(binding.root)
+    private val binding: EmptyItemBinding,
+) : RecyclerView.ViewHolder(binding.root)
--- feature/search/src/main/java/com/korea/search/dialog/model/BottomSheetItem.kt
+++ feature/search/src/main/java/com/korea/search/dialog/model/BottomSheetItem.kt
@@ -1,15 +1,15 @@
 package com.korea.search.dialog.model
 
 sealed class BottomSheetItem(
-    open val title: String
+    open val title: String,
 ) {
     data class ManufactureYear(
         override val title: String,
         val isSelected: Boolean,
-    ): BottomSheetItem(title)
+    ) : BottomSheetItem(title)
 
     data class ProductClassName(
         override val title: String,
         val isSelected: Boolean,
-    ): BottomSheetItem(title)
+    ) : BottomSheetItem(title)
 }
