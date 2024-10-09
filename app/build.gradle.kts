1585a6eb9dd7ffe8a8f5b06e69c994e093b8f6cd
jaehong <83493143+prk4224@users.noreply.github.com>
2024-10-09 11:35:47 +0900

    사용하지 않는 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.dagger.hilt)
     kapt(libs.dagger.hilt.compiler)
--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -33,7 +33,5 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    
 }
